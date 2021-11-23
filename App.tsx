0d3ffff3f1caaf9f1a93a13b3250f79eb134c1be
Tanmoy Sarker <tanmoysarker@Tanmoys-MacBook-Air.local>
2021-11-23 21:31:41 +0600

    redux initialized

--- App.tsx
+++ App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { View } from 'react-native';
 import Routes from './src/navigation/Routes';
+import { Provider } from 'react-redux';
+import { Store } from './src/redux/store';
 
 const App = () => {
 
   return (
-    <View style={{ flex: 1 }}>
-      <Routes />
-    </View>
+    <Provider store={Store}>
+      <View style={{ flex: 1 }}>
+          <Routes />
+      </View>
+    </Provider>
+   
   );
 };
 
