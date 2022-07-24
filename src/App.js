1eb2f216df32d8930703d245fbf4f4b76b8974de
Tanmoy Sarker <25175610+tanmoysarker@users.noreply.github.com>
2022-07-24 22:01:13 +0600

    app intro slider added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,114 @@
 import 'react-native-gesture-handler'
-import React from 'react'
+import React, {useState} from 'react'
+import {
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Image
+} from 'react-native'
 import MainNavigation from './navigation/navigation'
 import { Provider } from 'react-redux'
 import { store } from './stores/index'
+import AppIntroSlider from 'react-native-app-intro-slider';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+
+const slides = [
+  {
+    key: 1,
+    title: 'Title 1',
+    text: 'Description.\nSay something cool',
+    image: require('./assets/demo.jpeg'),
+    backgroundColor: '#59b2ab',
+  },
+  {
+    key: 2,
+    title: 'Title 2',
+    text: 'Other cool stuff',
+    image: require('./assets/demo.jpeg'),
+    backgroundColor: '#febe29',
+  },
+  {
+    key: 3,
+    title: 'Rocket guy',
+    text: 'I\'m already out of descriptions\n\nLorem ipsum bla bla bla',
+    image: require('./assets/demo.jpeg'),
+    backgroundColor: '#22bcb5',
+  }
+];
 
 const App = () => {
+  const [showRealApp, setShowRealApp] = useState(false);
+
+  const _renderItem = ({ item }) => {
+    return (
+      <View style={styles.slide}>
+        <Text style={styles.title}>{item.title}</Text>
+        <Image source={item.image} />
+        <Text style={styles.text}>{item.text}</Text>
+      </View>
+    );
+  }
+
+  const _renderNextButton = () => {
+    return (
+      <View style={styles.buttonCircle}>
+        <Icon
+          name="md-arrow-round-forward"
+          color="rgba(255, 255, 255, .9)"
+          size={24}
+        />
+      </View>
+    );
+  };
+
+  const _renderDoneButton = () => {
+    return (
+      <View style={styles.buttonCircle}>
+        <Icon
+          name="md-checkmark"
+          color="rgba(255, 255, 255, .9)"
+          size={24}
+        />
+      </View>
+    );
+  };
+
+  const _onDone = () => {
+    setShowRealApp(false);
+  }
+
   return (
-    <Provider store={store}>
-      <MainNavigation />
-    </Provider>
+    
+     <>
+     {showRealApp ?
+      <Provider store={store}>
+        <MainNavigation />
+      </Provider>
+       :
+       <AppIntroSlider 
+        renderItem={_renderItem} 
+        data={slides} onDone={_onDone} 
+        renderDoneButton={_renderDoneButton}
+        renderNextButton={_renderNextButton}
+       />
+   }
+   </>
   )
 }
+
+
+const styles = StyleSheet.create({
+  buttonCircle: {
+    width: 40,
+    height: 40,
+    backgroundColor: 'rgba(0, 0, 0, .2)',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
 
 export default App
